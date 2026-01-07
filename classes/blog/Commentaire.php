4df2514e5fbe88f576309fc7a0a8a36d284ff76a
hamzazirari <hamzazirari6002@gmail.com>
2026-01-07 11:33:55 +0100

    Feat : add blog classes

--- classes/blog/Commentaire.php
+++ classes/blog/Commentaire.php
@@ -15,9 +15,15 @@ class Commentaire {
     }
 
     // Getters & Setters
-    public function getId() { return $this->id; }
-    public function setId($id) { $this->id = $id; }
+    public function getId() {
+         return $this->id; }
 
-    public function getContenu() { return $this->contenu; }
-    public function setContenu($contenu) { $this->contenu = $contenu; }
+    public function setId($id) {
+         $this->id = $id; }
+
+    public function getContenu() {
+         return $this->contenu; }
+
+    public function setContenu($contenu) {
+         $this->contenu = $contenu; }
 }
--- classes/blog/Theme.php
+++ classes/blog/Theme.php
@@ -16,15 +16,23 @@ class Theme
     }
 
     // Getters o Setters
-    public function getId() { return $this->id; }
-    public function setId($id) { $this->id = $id; }
+    public function getId() { 
+        return $this->id; }
+    public function setId($id) { 
+        $this->id = $id; }
 
-    public function getTitre() { return $this->titre; }
-    public function setTitre($titre) { $this->titre = $titre; }
+    public function getTitre() {
+         return $this->titre; }
+    public function setTitre($titre) {
+         $this->titre = $titre; }
 
-    public function getDescription() { return $this->description; }
-    public function setDescription($description) { $this->description = $description; }
+    public function getDescription() {
+         return $this->description; }
+    public function setDescription($description) {
+         $this->description = $description; }
 
-    public function getActif() { return $this->actif; }
-    public function setActif($actif) { $this->actif = $actif; }
+    public function getActif() {
+         return $this->actif; }
+    public function setActif($actif) {
+         $this->actif = $actif; }
 }
